6112d991c4447a6a4d468517c152e20469a97952
pdvrostovSPb <pdvrostov@mail.ru>
2022-07-29 00:25:45 +0300

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿void PrintArray(string [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write (arr[i] + "   ");
+    }
+    Console.WriteLine();
+}
+string [] arr = new string [] {"1234", "1567", "-2", "Computer science", "555"};
+string [] mass = new string [arr.Length];
+void FillMass (string[] arr, string [] mass)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i].Length <= 3)
+        {
+            mass[count] = arr[i];
+            count++;
+        }
+    }
+}
+FillMass(arr, mass);
+PrintArray(mass);
